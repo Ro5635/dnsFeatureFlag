36fc1ca5f492ec5b5e777b60cafe7d02aa2dd8e7
Robert Curran <ro5635@gmail.com>
2022-03-05 22:58:51 +0000

    Catch on DNS Query Failure

--- dns_feature_flag/main.py
+++ dns_feature_flag/main.py
@@ -13,10 +13,13 @@ DNS_FEATURE_FLAG_BASE_DOMAIN = _get_default_base_domain()
 
 
 def is_flag_enabled(name, feature_flag_base_domain=DNS_FEATURE_FLAG_BASE_DOMAIN):
-    # ToDo: handle flag not found 
-    flag_uri = f"{name}.{feature_flag_base_domain}"
-    read_txt_records = dns.resolver.resolve(flag_uri, 'txt')
+    try:
+        flag_uri = f"{name}.{feature_flag_base_domain}"
+        read_txt_records = dns.resolver.resolve(flag_uri, 'txt')
 
-    # Ignoring possibility of multiple values 😛
-    # Assume that "1" means True else False
-    return read_txt_records[0].to_text() == '"1"'
+        # Ignoring possibility of multiple values 😛
+        # Assume that "1" means True else False
+        return read_txt_records[0].to_text() == '"1"'
+    except dns.resolver.NXDOMAIN:
+        print('Feature Flag Not Found, defaulting to False')
+        return False
